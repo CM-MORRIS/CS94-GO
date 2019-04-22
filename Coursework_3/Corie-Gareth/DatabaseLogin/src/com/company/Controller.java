d53a97326aa13742a5aebfe973c032744bff002b
gartho95 <48065980+gartho95@users.noreply.github.com>
2019-04-22 19:27:49 +0100

    Add files via upload
    added back button and leaderboard button

--- Coursework_3/Corie-Gareth/DatabaseLogin/src/com/company/Controller.java
+++ Coursework_3/Corie-Gareth/DatabaseLogin/src/com/company/Controller.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -10,6 +12,9 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+
+
+
 import java.io.IOException;
 
 public class Controller {
@@ -18,10 +23,17 @@ public class Controller {
     public Button btnLogin;
 
     @FXML
+    public Button btnLdr;
+
+    @FXML
+    public Button backDash;
+
+    @FXML
     public TextField usertxt;
 
     @FXML
     public PasswordField pswdtxt;
+
 
 
     public void onLoginClick() {
@@ -47,6 +59,44 @@ public class Controller {
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
+                }
+            }
+        });
+    }
+    public void onLdrClick() {
+
+        btnLdr.setOnAction(new EventHandler<ActionEvent>() {
+             @Override
+             public void handle(ActionEvent event) {
+                 try {
+                     Parent Leaderboard;
+                     Leaderboard = FXMLLoader.load(getClass().getResource("Leaderboard.fxml"));
+                     Stage mainStage;
+                     mainStage = GUI.parentWindow;
+                     mainStage.getScene().setRoot(Leaderboard);
+                     mainStage.show();
+                 } catch (IOException e) {
+                     e.printStackTrace();
+                 }
+             }
+         });
+
+    }
+    public void onBackClick() {
+        //provides functionality to go back to the dashboard from the leaderboatd
+
+        backDash.setOnAction(new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent back) {
+                try {
+                    Parent dashboard;
+                    dashboard = FXMLLoader.load(getClass().getResource("dashboard.fxml"));
+                    Stage mainStage;
+                    mainStage = GUI.parentWindow;
+                    mainStage.getScene().setRoot(dashboard);
+                    //mainStage.setResizable(false);
+                    mainStage.show();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
         });
