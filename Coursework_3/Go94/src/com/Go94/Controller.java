e2fe712421ad0f0236bca88e94e2eaa7ff07b330
gartho95 <48065980+gartho95@users.noreply.github.com>
2019-04-25 13:08:47 +0100

    Update Controller.java
    javadoc

--- Coursework_3/Go94/src/com/Go94/Controller.java
+++ Coursework_3/Go94/src/com/Go94/Controller.java
@@ -28,24 +28,49 @@ import java.util.Date;
 import java.text.SimpleDateFormat;
 import javax.swing.*;
 
+/**
+ * The Controller that deals with opening the initial login screen
+ *
+ * @author Corie Morris and Gareth Thomas
+ */
 public class Controller {
 
+    /**
+     * Creating the button to click to login
+     */
     @FXML
     public Button btnLogin;
     
+    /**
+     * The area to enter the username
+     */
     @FXML
     public TextField usertxt;
 
+    /**
+     * The area to enter the password
+     */
     @FXML
     public PasswordField pswdtxt;
 
+    /**
+     * To get the date from the UserDB class of last time logged in
+     */
     public static String loggedUsr;
     
+    /**
+     * String to send what username to get the info of last logged in from the database
+     */
     public static String username;
     
+    /**
+     * The string for what to display the return the last login from the database.
+     */
     public static String lastLogin;
 
-
+    /**
+     * What happens when the actual login button is clicked successfully.
+     */
     public void onLoginClick() throws SQLException {
         System.out.println("Login button pressed");
         // instructions on button press. Only logs user in if credentials match.
