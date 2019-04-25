c21f6b002cefd99c4fa707421f87c02b56452fae
gartho95 <48065980+gartho95@users.noreply.github.com>
2019-04-25 13:37:21 +0100

    Update LeaderBoardData.java
    javadocs...

--- Coursework_3/Go94/src/com/Go94/LeaderBoardData.java
+++ Coursework_3/Go94/src/com/Go94/LeaderBoardData.java
@@ -2,21 +2,44 @@ package com.Go94;
 
 import javafx.beans.property.*;
 
+/**
+ * The class which allows for the data from the database to be used within the javafx tables
+ *
+ * @author Corie Morris
+ */
 public class LeaderBoardData {
 
+    /**
+     * Each variable which will allow for a property to show for each element of the leaderboard
+     */
     private StringProperty username = new SimpleStringProperty();
     private IntegerProperty wins = new SimpleIntegerProperty();
     private IntegerProperty loss = new SimpleIntegerProperty();
     private IntegerProperty winPercentage = new SimpleIntegerProperty();
 
+    /**
+     * Essentially the same 3 functions for each element needing to be displayed
+     *
+     * @return the username from the DB
+     */
     public StringProperty usernameProperty() {
         return username;
     }
 
+    /**
+     * Gets the property for the initial username and returns as a string
+     *
+     * @return the username after passed through the DB
+     */
     public String getUsername() {
         return username.get();
     }
 
+    /**
+     * Finally sets name as the username after it has been passed to a string.
+     *
+     * @param name what will be set the username to be displayed.
+     */
     public void setUsername(String name) {
         this.username.set(name);
     }
